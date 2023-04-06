3af2ec21215442a2e631886810df16ad5d0e3d89
taic-dev <c.t.o.taishi.0530@gmail.com>
2023-04-06 15:23:39 +0900

    add zh

--- src/formatObj.ts
+++ src/formatObj.ts
@@ -11,6 +11,10 @@ type formatType = {
     w: Array<string>,
     m: Array<string>,
   }
+  zh: {
+    w: Array<string>,
+    m: Array<string>,
+  }
 }
 
 export const formatObj:formatType = {
@@ -26,4 +30,8 @@ export const formatObj:formatType = {
     w: ["일요일","월요일","화요일","수요일","목요일","금요일","토요일",],
     m: ["일월","이월","삼월","사월","오월","유월","칠월","팔월","구월","시월","십일월","십이월",]
   },
+  zh: {
+    w: ["星期 天","星期 一","星期 二","星期 三","星期 四","星期 五","星期 六",],
+    m: ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月",]
+  }
 };
