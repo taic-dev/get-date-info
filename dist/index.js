fd0e43dbd47fd5639c9d8b07c46ebccaec26dc48
taic-dev <c.t.o.taishi.0530@gmail.com>
2023-04-06 15:11:49 +0900

    version up

--- dist/index.js
+++ dist/index.js
@@ -2,10 +2,10 @@
 const { formatObj } = require("./formatObj");
 const { initialObj } = require("./initialObj");
 function getDateInfo(initial, format) {
-  if (initial === "w" || initial === "m") {
-    return formatObj[format][initial][initialObj[initial]];
-  }
-  return initialObj[initial];
+    if (initial === "w" || initial === "m") {
+        return formatObj[format][initial][initialObj[initial]];
+    }
+    return initialObj[initial];
 }
 module.exports = getDateInfo;
 module.exports.default = getDateInfo;
